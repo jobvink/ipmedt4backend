1ecade97349e6bb39cc9ba69c1a807931bd91613
Job Vink <jobvink@Jobs-Mac-mini.local>
2017-06-10 19:07:58 +0200

    routing en autenticatie

--- ipmedt4/routes/web.php
+++ ipmedt4/routes/web.php
@@ -14,3 +14,36 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => '/docter/{docter}'], function (){
+    Route::get('/', 'DocterController@dashboard');
+    Route::get('/dashboard', 'DocterController@dashboard');
+    Route::group(['prefix' => '/patients/{patient}'], function() {
+        Route::get('/', 'PatientController@show');
+        Route::group(['prefix' => '/appointments'], function() {
+            Route::get('/create', 'AppointmentControler@create');
+            Route::post('/','AppointmentController@store');
+            Route::get('/{appointment}/edit', 'AppointmentController@edit');
+            Route::patch('/{appointment}', 'AppointmentController@update');
+            Route::delete('/{appointment}', 'AppointmetnController@destroy');
+        });
+        Route::group(['prefix' => '/consults'], function(){
+            Route::get('/create', 'ConsultController@create');
+            Route::post('/', 'ConsultController@store');
+            Route::get('/{consult}/edit', 'ConsultController@edit');
+            Route::patch('/{consult}', 'ConsultController@update');
+            Route::delete('{consult}', 'ConsultController@destroy');
+        });
+    });
+});
+
+Route::group(['prefix' => '/patient/{patient}'], function(){
+    Route::get('/', 'PatientControllter@show');
+    Route::get('/dashboard', 'PatientController@show');
+});
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
